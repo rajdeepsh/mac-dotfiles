6ba46aca71169c5d9c608ebe93bbf536312ae798
rajdeepsh <rajdeepsh@icloud.com>
2025-07-29 18:19:20 +0800

    mod config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,7 @@ if status is-interactive
 end
 /opt/homebrew/bin/brew shellenv | source
 set -g fish_greeting
+set -gx XDG_CONFIG_HOME $HOME/.config
+
+alias sw="cd $HOME/mac-dotfiles && stow --adopt */ && git reset --hard HEAD && cd -"
+alias swn="cd $HOME/mac-dotfiles && stow --adopt -nv */ && cd -"
