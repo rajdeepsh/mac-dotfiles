1dcd64f60a6569d0aba1f9ebf16da1ba8eb485b2
rajdeepsh <rajdeepsh@icloud.com>
2025-07-29 18:26:04 +0800

    mod config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,14 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Sources
 /opt/homebrew/bin/brew shellenv | source
+
+# Variables
 set -g fish_greeting
 set -gx XDG_CONFIG_HOME $HOME/.config
 
+# Aliases
 alias sw="cd $HOME/mac-dotfiles && stow --adopt */ && git reset --hard HEAD && cd -"
 alias swn="cd $HOME/mac-dotfiles && stow --adopt -nv */ && cd -"
