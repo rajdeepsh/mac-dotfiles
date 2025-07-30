c480acd24b7b1e3f0c9ab3e05d6324b17a7afbfe
rajdeepsh <rajdeepsh@icloud.com>
2025-07-30 19:47:04 +0800

    mod fish

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -3,12 +3,12 @@ function fish_prompt
                 echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
         end
     
-        echo -n (set_color blue)(prompt_pwd)' '
+        echo -n (set_color f5e0dc)(prompt_pwd)' '
     
         set_color -o
         if fish_is_root_user
                 echo -n (set_color red)'# '
         end
-        echo -n (set_color green)'❯ '
+        echo -n (set_color f5e0dc)'❯ '
         set_color normal
 end
