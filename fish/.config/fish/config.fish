54df3bb3cbeb20546f65d69aea8cc8e30926868f
Rajdeep <rajd33psh@gmail.com>
2026-08-07 18:41:59 +0800

    Simplify eza aliases and split listing variants

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,8 +9,10 @@ alias c opencode
 alias g lazygit
 alias v nvim
 alias s stowdots
-alias ls "eza -l -F never --color always --icons always -s name --group-directories-first --show-symlinks -h -o --no-user --total-size --no-git"
-alias tree "eza -l -F never --color always --icons always -s name --group-directories-first --show-symlinks -h -o --no-user --total-size --no-git -T"
+alias ls "eza -l -F never --color always --icons always -s name --group-directories-first --show-symlinks -h -o"
+alias lsa "ls -a"
+alias lt "ls -T"
+alias lta "lt -a"
 
 fzf --fish | source
 
