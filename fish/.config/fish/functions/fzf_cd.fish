2dd9d1386adf262e544b9f8ce2e7a8ec5ab78205
Rajdeep <rajd33psh@gmail.com>
2026-08-07 18:11:23 +0800

    Implement fzf_cd with home directory walker and skip list

--- fish/.config/fish/functions/fzf_cd.fish
+++ fish/.config/fish/functions/fzf_cd.fish
@@ -1,2 +1,10 @@
 function fzf_cd
+    set -l selected (command fzf \
+        --walker dir,follow,hidden \
+        --walker-root $HOME \
+        --walker-skip .git,node_modules,Library,Movies,Music,Pictures,Public)
+
+    if test -n "$selected"
+        cd "$selected"
+    end
 end
